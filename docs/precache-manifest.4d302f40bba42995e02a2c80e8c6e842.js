8cc8fc4c06ddb3b618ebc324b59c19e70e821ac8
Clavianus Juneardo <juneardoc@gmail.com>
2019-07-18 23:27:33 +0700

    change package.json scripts & change pathname to relative

---
+++ docs/precache-manifest.4d302f40bba42995e02a2c80e8c6e842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69197f7be3d59e8b0c9523fd96a69ca4",
+    "url": "/freshman-project/index.html"
+  },
+  {
+    "revision": "92aa6ffa249edd7d763a",
+    "url": "/freshman-project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/freshman-project/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "92aa6ffa249edd7d763a",
+    "url": "/freshman-project/static/js/main.d30b1173.chunk.js"
+  },
+  {
+    "revision": "66d963d7f06185b66640",
+    "url": "/freshman-project/static/js/runtime~main.6eb0defb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/freshman-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/freshman-project/precache-manifest.4d302f40bba42995e02a2c80e8c6e842.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/freshman-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
